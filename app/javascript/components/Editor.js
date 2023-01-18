1a862c54b09cbbc9494206c4cda0dca5ec5fb71a
ToshiyaEgusa <heinrich402@gmail.com>
2023-01-18 16:16:57 +0900

    作成機能の追加

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Header from './Header'
 import EventList from './EventList'
 import Event from './Event'
+import EventForm from './EventForm'
 
 const Editor = () => {
   const [events, setEvents] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [isError, setIsError] = useState(false)
+  const navigate = useNavigate()
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -24,21 +27,47 @@ const Editor = () => {
     }
     fetchData()
   }, [])
+
+  const addEvent = async (newEvent) => {
+    try {
+      const response = await window.fetch('/api/events', {
+        method: 'POST',
+        body: JSON.stringify(newEvent),
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+      if (!response.ok) throw Error(response.statusText)
+
+      const savedEvent = await response.json()
+      const newEvents = [...events, savedEvent]
+      setEvents(newEvents)
+      window.alert('Event Added!')
+      navigate(`/events/${savedEvent.id}`)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <>
       <Header />
-      {isError && <p>Something went wrong. Check the console.</p>}
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <EventList events={events} />
+      <div className="grid">
+        {isError && <p>Something went wrong. Check the console.</p>}
+        {isLoading ? (
+          <p className="loading">Loading...</p>
+        ) : (
+          <>
+            <EventList events={events} />
 
-          <Routes>
-            <Route path=":id" element={<Event events={events} />} />
-          </Routes>
-        </>
-      )}
+            <Routes>
+              <Route path="new" element={<EventForm onSave={addEvent} />} />
+              <Route path=":id" element={<Event events={events} />} />
+            </Routes>
+          </>
+        )}
+      </div>
     </>
   )
 }
--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 const EventList = ({ events }) => {
   const renderEvents = (eventArray) => {
     eventArray.sort((a, b) => new Date(b.event_date) - new Date(a.event_date))
+
     return eventArray.map((event) => (
       <li key={event.id}>
-        <Link to={`/events/${event.id}`}>
+        <NavLink to={`/events/${event.id}`}>
           {event.event_date}
           {' - '}
           {event.event_type}
-        </Link>
+        </NavLink>
       </li>
     ))
   }
   return (
-    <section>
-      <h2>Events</h2>
+    <section className="eventList">
+      <h2>
+        Events
+        <Link to="/events/new">New Event</Link>
+      </h2>
       <ul>{renderEvents(events)}</ul>
     </section>
   )
